cd6d23a36ba19f93e68642cdce1d6718b2f0d572
True._.Mary <94931275+True0O0Mary@users.noreply.github.com>
2022-06-28 21:46:36 +0300

    Create (LX8.1-task2)proc.sh

---
+++ (LX8.1-task2)proc.sh
@@ -0,0 +1,18 @@
+!/usr/bin/bash
+IFS='/n'
+echo "<html>" >>index.html
+	 
+echo "<title> Process statistic </title>" >>index.html
+
+echo "<body>" >>index.html
+echo "<h1>Distribution of processes</h1>" >>index.html
+
+for file in *.html
+do
+echo $file >>index.html
+echo "<hr noshade>" >>index.html
+filename=${file%.*}
+echo $filename.total  >>index.html
+done
+echo "</body>" >>index.html
+echo "</html>" >>index.html
