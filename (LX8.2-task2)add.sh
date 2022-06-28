8410b53cdd0a613a1a511f0dd605d3c326685930
True._.Mary <94931275+True0O0Mary@users.noreply.github.com>
2022-06-28 21:53:06 +0300

    Create (LX8.2-task2)add.sh

---
+++ (LX8.2-task2)add.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+read -p "Input process code" cod
+IFS=$'\n'
+for string in $(cat $"/tmp/tim/log.txt")
+do
+str=$string
+host=`echo $str | cut -d " " -f 1`
+times=`echo $str | cut -d "[" -f 2 | cut -d "]" -f 1`
+request=`echo $str | cut -d "\"" -f 2 |  cut -d "\"" -f 1`
+type=`echo $request | awk '{print $1}'`
+host_link=`echo $request | awk '{print $2}'`
+protocol=`echo $request | awk '{print $3}'`
+code=`echo $str | cut -d "\"" -f 3 |  cut -d "\"" -f 2`
+answer_code=`echo $code | awk '{print $1}'`
+bite=`echo $code | awk '{print $2}'`
+link_site=`echo $str | cut -d "\"" -f 4 |  cut -d "\"" -f 3`
+	 
+client_info=`echo $str | cut -d "\"" -f 6 |  cut -d "\"" -f 5`
+program=`echo $client_info | awk '{print $1}'`
+os=`echo $client_info | awk '{print $3}' | cut -d ";" -f 2 | cut -d ";" -f 1`
+if [ "$answer_code" -eq "$cod" ]; then
+echo "$times from $host according to the protocol $protocol request was executed type $type to get the resourse located at the link $host_link. The response code to the request from server: $answer_code. $bite bytes passed. The request was transmitted from the site $link_site. The client used program $program, client\`s OS: $os"
+fi
+done
+	
+	
+	
